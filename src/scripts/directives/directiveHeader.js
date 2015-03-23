e2f44987303322308b445baf4c7d514322dd43cf
ohager <oliver.hager@dextra-sw.com>
2015-03-23 15:51:05 -0300

    Header

--- src/scripts/directives/directiveHeader.js
+++ src/scripts/directives/directiveHeader.js
@@ -6,7 +6,16 @@
             return {
                 templateUrl: "dist/templates/directives/dx-header.html",
                 link :function ($scope) {
+                    $scope.user = {
+                        ref : '#',
+                        imgUrl : "dist/images/user.jpg",
+                        name : "Chuck Norris",
+                        company : "Acme Unlimited"
+                    };
 
+                    $scope.notifications = {
+                        count : 13
+                    }
                 }
             };
         });
